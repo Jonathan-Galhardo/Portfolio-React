0be307f96786c624d5250fa7335c1f8bdb9fb24a
Jonathan-Galhardo <galhardoj@gmail.com>
2022-08-23 08:48:41 -0300

    Conteúdo Header

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CV from '../../assets/cv.pdf'
+
+export const CTA = () => {
+  return (
+    <div className='cta'>
+      <a href={CV} download>Download CV</a>
+      <a href="#contact">Contato</a>
+    </div>
+  )
+}
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './header.css'
+import { CTA } from "./CTA";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Olá, Eu sou! </h5>
+        <h1>Jonathan Galhardo</h1>
+        <div className="text-light">Desenvolvedor Frontend</div>
+        <CTA />
+      </div>
+    </header>
   )
 }
 
