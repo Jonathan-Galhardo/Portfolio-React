fd44b8462b950e16401085bd0f6060a29751652f
Jonathan-Galhardo <galhardoj@gmail.com>
2022-08-25 09:03:33 -0300

    CSS Seção Experiência

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -8,7 +8,7 @@ import { DiJavascript1 } from "react-icons/di"
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience
+    <section id='experience'>
       <h5>Skills</h5>
       <h2>Tecnologias</h2>
 
@@ -18,31 +18,41 @@ const Experience = () => {
           <div className='experience__content'>
             <article className='experiencie__details'>
               <AiOutlineHtml5 />
-              <h4>HTML5</h4>
-              <small className='text-light'>Avançado</small>
+              <div>
+                <h4>HTML5</h4>
+                <small className='text-light'>Avançado</small>
+              </div>
             </article>
             <article className='experiencie__details'>
               <TbBrandCss3 />
-              <h4>CSS3</h4>
-              <small className='text-light'>Avançado</small>
+              <div>
+                <h4>CSS3</h4>
+                <small className='text-light'>Avançado</small>
+              </div>
             </article>
 
             <article className='experiencie__details'>
               <TbBrandBootstrap />
-              <h4>Bootstrap</h4>
-              <small className='text-light'>Avançado</small>
+              <div>
+                <h4>Bootstrap</h4>
+                <small className='text-light'>Avançado</small>
+              </div>
             </article>
 
             <article className='experiencie__details'>
               <DiJavascript1 />
-              <h4>JavaScript</h4>
-              <small className='text-light'>Intermediário</small>
+              <div>
+                <h4>JavaScript</h4>
+                <small className='text-light'>Intermediário</small>
+              </div>
             </article>
 
             <article className='experiencie__details'>
               <FaReact />
-              <h4>React</h4>
-              <small className='text-light'>Básico</small>
+              <div>
+                <h4>React</h4>
+                <small className='text-light'>Básico</small>
+              </div>
             </article>
           </div>
         </div>
