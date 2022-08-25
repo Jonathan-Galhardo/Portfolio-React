0d32abc838e0212c363a04ef6a3ea6291de4f118
Jonathan-Galhardo <galhardoj@gmail.com>
2022-08-25 15:58:20 -0300

    Seção Portfólio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,76 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from "../../assets/section_depoiments.png"
+import IMG2 from "../../assets/calc_gorjeta.png"
+import IMG3 from "../../assets/clt_pj.png"
+import IMG4 from "../../assets/calc.png"
+import IMG5 from "../../assets/valhalla.png"
+import IMG6 from "../../assets/orcamento_pessoal.jpg"
 
 const Portfolio = () => {
   return (
-    <section>Portfolio</section>
+    <section id='portfolio'>
+      <h5>Trabalhos recentes</h5>
+      <h2>Portfólio</h2>
+
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG6} alt="" />
+          </div>
+          <h3>Tile</h3>
+          <a href="#" className='btn' target='_blank'>GitHub</a>
+          <a href="#" className='btn btn-primary' target='_blank'>Deploy</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG5} alt="" />
+          </div>
+          <h3>Tile</h3>
+          <a href="#" className='btn' target='_blank'>GitHub</a>
+          <a href="#" className='btn btn-primary' target='_blank'>Deploy</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG4} alt="" />
+          </div>
+          <h3>Tile</h3>
+          <a href="#" className='btn' target='_blank'>GitHub</a>
+          <a href="#" className='btn btn-primary' target='_blank'>Deploy</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG3} alt="" />
+          </div>
+          <h3>Tile</h3>
+          <a href="#" className='btn' target='_blank'>GitHub</a>
+          <a href="#" className='btn btn-primary' target='_blank'>Deploy</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG2} alt="" />
+          </div>
+          <h3>Tile</h3>
+          <a href="#" className='btn' target='_blank'>GitHub</a>
+          <a href="#" className='btn btn-primary' target='_blank'>Deploy</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>Tile</h3>
+          <a href="#" className='btn' target='_blank'>GitHub</a>
+          <a href="#" className='btn btn-primary' target='_blank'>Deploy</a>
+        </article>
+
+      </div>
+
+    </section>
   )
 }
 
