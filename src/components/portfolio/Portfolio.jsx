979341b4eefbb697ac83ac255b6fd5e265e1657f
Jonathan-Galhardo <galhardoj@gmail.com>
2022-08-25 16:44:53 -0300

    CSS Seção Portfólio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -12,60 +12,71 @@ const Portfolio = () => {
     <section id='portfolio'>
       <h5>Trabalhos recentes</h5>
       <h2>Portfólio</h2>
-
       <div className='container portfolio__container'>
         <article className='portfolio__item'>
           <div className='portfolio__item-image'>
             <img src={IMG6} alt="" />
           </div>
-          <h3>Tile</h3>
-          <a href="#" className='btn' target='_blank'>GitHub</a>
-          <a href="#" className='btn btn-primary' target='_blank'>Deploy</a>
+          <h3>Orçamento Pessoal</h3>
+          <div className='portfolio__item-cta'>
+            <a href="https://github.com/Jonathan-Galhardo/Orcamento-Pessoal" className='btn' target='_blank' rel="noreferrer">GitHub</a>
+            <a href="#" className='btn btn-primary' target='_blank'>Deploy</a>
+          </div>
         </article>
 
         <article className='portfolio__item'>
           <div className='portfolio__item-image'>
             <img src={IMG5} alt="" />
           </div>
-          <h3>Tile</h3>
-          <a href="#" className='btn' target='_blank'>GitHub</a>
-          <a href="#" className='btn btn-primary' target='_blank'>Deploy</a>
+          <h3>Game: Valhalla Glória Eterna</h3>
+          <div className='portfolio__item-cta'>
+            <a href="https://github.com/Jonathan-Galhardo/Valhalla_Gloria_Eterna" className='btn' target='_blank' rel="noreferrer">GitHub</a>
+            <a href="https://valhalla-batalha-final.vercel.app/" className='btn btn-primary' target='_blank' rel="noreferrer">Deploy</a>
+          </div>
         </article>
 
         <article className='portfolio__item'>
           <div className='portfolio__item-image'>
             <img src={IMG4} alt="" />
           </div>
-          <h3>Tile</h3>
-          <a href="#" className='btn' target='_blank'>GitHub</a>
-          <a href="#" className='btn btn-primary' target='_blank'>Deploy</a>
+          <h3>Calculadora em Js</h3>
+          <div className='portfolio__item-cta'>
+            <a href="https://github.com/Jonathan-Galhardo/Calculadora_JavaScript" className='btn' target='_blank' rel="noreferrer">GitHub</a>
+            <a href="https://calculadora-java-script-mu.vercel.app/" className='btn btn-primary' target='_blank' rel="noreferrer">Deploy</a>
+          </div>
         </article>
 
         <article className='portfolio__item'>
           <div className='portfolio__item-image'>
             <img src={IMG3} alt="" />
           </div>
-          <h3>Tile</h3>
-          <a href="#" className='btn' target='_blank'>GitHub</a>
-          <a href="#" className='btn btn-primary' target='_blank'>Deploy</a>
+          <h3>Clt ou Pj? </h3>
+          <div className='portfolio__item-cta'>
+            <a href="https://github.com/Jonathan-Galhardo/CLT-ou-PJ" className='btn' target='_blank' rel="noreferrer">GitHub</a>
+            <a href="https://clt-ou-pj.vercel.app/" className='btn btn-primary' target='_blank' rel="noreferrer">Deploy</a>
+          </div>
         </article>
 
         <article className='portfolio__item'>
           <div className='portfolio__item-image'>
             <img src={IMG2} alt="" />
           </div>
-          <h3>Tile</h3>
-          <a href="#" className='btn' target='_blank'>GitHub</a>
-          <a href="#" className='btn btn-primary' target='_blank'>Deploy</a>
+          <h3>Rachando a Conta</h3>
+          <div className='portfolio__item-cta'>
+            <a href="https://github.com/Jonathan-Galhardo/Calculadora-de-Gorjetas" className='btn' target='_blank' rel="noreferrer">GitHub</a>
+            <a href="https://calculadora-de-gorjetas-three.vercel.app/" className='btn btn-primary' target='_blank' rel="noreferrer">Deploy</a>
+          </div>
         </article>
 
         <article className='portfolio__item'>
           <div className='portfolio__item-image'>
             <img src={IMG1} alt="" />
           </div>
-          <h3>Tile</h3>
-          <a href="#" className='btn' target='_blank'>GitHub</a>
-          <a href="#" className='btn btn-primary' target='_blank'>Deploy</a>
+          <h3>Seção Depoimentos</h3>
+          <div className='portfolio__item-cta'>
+            <a href="https://github.com/Jonathan-Galhardo/Secao-Depoimentos" className='btn' target='_blank' rel="noreferrer">GitHub</a>
+            <a href="https://secao-depoimentos.vercel.app/" className='btn btn-primary' target='_blank' rel="noreferrer">Deploy</a>
+          </div>
         </article>
 
       </div>
