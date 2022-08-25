0164cd95cdd0c2df96627dfa604e26be3d1a3085
Jonathan-Galhardo <galhardoj@gmail.com>
2022-08-25 09:40:04 -0300

    Seção Serviços

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 import './services.css'
+import { BsCheck2Circle } from "react-icons/bs";
 
 const Services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>O que eu faço</h5>
+      <h2>Serviços</h2>
+
+      <div className='container services__container'>
+        <article className='service'>
+          <div className='service__head'>
+            <h3>Desenvolvimento de Interface</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BsCheck2Circle className='service__list-icon' />
+              <p>ACESSIBILIDADE: uso correto das semânticas HTML.</p>
+            </li>
+            <li>
+              <BsCheck2Circle className='service__list-icon' />
+              <p>RESPONSIVIDADE: facilidade na navegação em todos os navegadores e dispositivos.</p>
+            </li>
+            <li>
+              <BsCheck2Circle className='service__list-icon' />
+              <p>PERFORMANCE: Código limpo e eficiente, velocidade, peso de página, tempo de carregamento total.</p>
+            </li>
+          </ul>
+
+        </article>
+      </div>
+
+
+
+
+
+    </section>
   )
 }
 
