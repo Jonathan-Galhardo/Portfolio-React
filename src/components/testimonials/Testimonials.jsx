0329ff3972d3e4534a9bf8431b1e591c4187e7c4
Jonathan-Galhardo <galhardoj@gmail.com>
2022-08-25 17:12:37 -0300

    Seção Depoimentos

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,58 @@
 import React from 'react'
 import './testimonials.css'
+import AVAT1 from '../../assets/avatar1.jpg'
+import AVAT2 from '../../assets/avatar2.jpg'
+import AVAT3 from '../../assets/avatar3.jpg'
+import AVAT4 from '../../assets/avatar4.jpg'
 
 const Testimonials = () => {
   return (
-    <section>Testimonials</section>
+    <section id='testimonials'>
+      <h5>O que os clientes falam</h5>
+      <h2>Depoimentos</h2>
+
+      <div className='container testimonials__container'>
+        <article className='testimonial'>
+          <div className="client__avatar">
+            <img src={AVAT1} alt="Avt1" />
+            <h5 className='client__name'>Nome Cliente</h5>
+            <small className='client__review'>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus ea rerum fuga, reiciendis voluptatibus totam, optio impedit pariatur obcaecati dolores quod iste provident voluptatum consequuntur, laborum accusantium? Fugit, molestiae deserunt!
+            </small>
+          </div>
+        </article>
+
+        <article className='testimonial'>
+          <div className="client__avatar">
+            <img src={AVAT1} alt="Avt1" />
+            <h5 className='client__name'>Nome Cliente</h5>
+            <small className='client__review'>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus ea rerum fuga, reiciendis voluptatibus totam, optio impedit pariatur obcaecati dolores quod iste provident voluptatum consequuntur, laborum accusantium? Fugit, molestiae deserunt!
+            </small>
+          </div>
+        </article>
+
+        <article className='testimonial'>
+          <div className="client__avatar">
+            <img src={AVAT1} alt="Avt1" />
+            <h5 className='client__name'>Nome Cliente</h5>
+            <small className='client__review'>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus ea rerum fuga, reiciendis voluptatibus totam, optio impedit pariatur obcaecati dolores quod iste provident voluptatum consequuntur, laborum accusantium? Fugit, molestiae deserunt!
+            </small>
+          </div>
+        </article>
+
+        <article className='testimonial'>
+          <div className="client__avatar">
+            <img src={AVAT1} alt="Avt1" />
+            <h5 className='client__name'>Nome Cliente</h5>
+            <small className='client__review'>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus ea rerum fuga, reiciendis voluptatibus totam, optio impedit pariatur obcaecati dolores quod iste provident voluptatum consequuntur, laborum accusantium? Fugit, molestiae deserunt!
+            </small>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
