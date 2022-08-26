f7de069be58d787297520e664e2013404a53babf
Jonathan-Galhardo <galhardoj@gmail.com>
2022-08-26 15:35:14 -0300

    Links Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,6 +5,18 @@ const Footer = () => {
   return (
     <footer>
       <a href="#" className='footer__logo'>Jonathan Galhardo</a>
+
+
+      <ul className='links'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>Bio</a></li>
+        <li><a href='#experience'>Tecnologias</a></li>
+        <li><a href='#services'>Serviços</a></li>
+        <li><a href='#portfolio'>Portfólio</a></li>
+        <li><a href='#testimonials'>Depoimentos</a></li>
+        <li><a href='#contact'>Contatos</a></li>
+
+      </ul>
     </footer>
   )
 }
