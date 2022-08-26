1ebe6b9c713c572a65b17dbfa31e9a9ed45c417c
Jonathan-Galhardo <galhardoj@gmail.com>
2022-08-26 15:51:21 -0300

    Seção Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './footer.css'
+import { SiGmail } from "react-icons/si";
+import { SiWhatsapp } from "react-icons/si";
+import { GrLinkedin } from "react-icons/gr";
 
 const Footer = () => {
   return (
@@ -15,8 +18,17 @@ const Footer = () => {
         <li><a href='#portfolio'>Portfólio</a></li>
         <li><a href='#testimonials'>Depoimentos</a></li>
         <li><a href='#contact'>Contatos</a></li>
-
       </ul>
+
+      <div className='footer__socials'>
+        <a href="mailto:galhardoj@gmail.com"><SiGmail /></a>
+        <a href="https://api.whatsapp.com/send?phone=5555984389850&text=Ol%C3%A1!"><SiWhatsapp /></a>
+        <a href="https://www.linkedin.com/in/jonathangalhardo/"><GrLinkedin /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Jonathan Galhardo. Todos os direitos reservados.</small>
+      </div>
     </footer>
   )
 }
